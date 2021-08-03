6b982875adf5a6d9d7e4811d806d5cdb7ac1479f
FusingBlaze <fusingblaze8@gmail.com>
2021-08-03 12:39:30 +0200

    cambios

--- Assets/scripts/PlayerMov.cs
+++ Assets/scripts/PlayerMov.cs
@@ -2,39 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkelletonMov : MonoBehaviour
+public class PlayerMov : MonoBehaviour
 {
-    public float speed=2;
-    
-
+    public float speed = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Mov();
+        ;
     }
     public void Mov()
     {
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + new Vector3(0, 1, 0) * speed * Time.deltaTime;
+
         }
-        
+
         if (Input.GetKey(KeyCode.D))
         {
             transform.position = transform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
         }
-        
+
         if (Input.GetKey(KeyCode.S))
         {
             transform.position = transform.position + new Vector3(0, -1, 0) * speed * Time.deltaTime;
         }
-         
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position + new Vector3(-1, 0, 0) * speed * Time.deltaTime;

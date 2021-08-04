acb4c0ee3e060df8afd242911dda8d86869f6bd6
FusingBlaze <fusingblaze8@gmail.com>
2021-08-04 11:04:02 +0200

    movimiento personaje

--- Assets/scripts/PlayerMov.cs
+++ Assets/scripts/PlayerMov.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMov : MonoBehaviour
 {
     public float speed = 2;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,22 +23,54 @@ public class PlayerMov : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.position = transform.position + new Vector3(0, 1, 0) * speed * Time.deltaTime;
+            animator.SetBool("izq", false);
+            animator.SetBool("derecha", false);
+            animator.SetBool("abajo", false);
+                animator.SetBool("arriba", true);
 
+        }
+        else
+        {
+            animator.SetBool("arriba", false);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.position = transform.position + new Vector3(1, 0, 0) * speed * Time.deltaTime;
+            animator.SetBool("derecha", true);
+            animator.SetBool("arriba", false);
+            animator.SetBool("izq", false);
+            animator.SetBool("abajo", false);
+        }
+        else
+        {
+            animator.SetBool("derecha", false);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.position = transform.position + new Vector3(0, -1, 0) * speed * Time.deltaTime;
+            animator.SetBool("abajo", true);
+            animator.SetBool("arriba", false);
+            animator.SetBool("izq", false);
+            animator.SetBool("derecha", false);
+        }
+        else
+        {
+            animator.SetBool("abajo", false);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position + new Vector3(-1, 0, 0) * speed * Time.deltaTime;
+            animator.SetBool("izq", true);
+            animator.SetBool("arriba", false);
+            animator.SetBool("derecha", false);
+            animator.SetBool("abajo", false);
+        }
+        else
+        {          
+            animator.SetBool("izq", false);     
         }
     }
 }

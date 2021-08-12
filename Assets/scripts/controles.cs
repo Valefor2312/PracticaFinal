647f65143455c98785aac24f8ae45fefa00bc3c2
Ángel Rafael Rubio Fernández <anrubiof@Port28VS>
2021-08-12 12:19:54 +0200

    de toh

---
+++ Assets/scripts/controles.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controles : MonoBehaviour
+
+{
+
+  public void PlayGame()
+  {
+    SceneManager.LoadScene(2);
+  }
+
+  public void QuitGame()
+  {
+    Application.Quit();
+
+  }
+}
